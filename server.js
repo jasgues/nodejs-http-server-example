cbcf9dd15546e2fcda98763a1d55a3e0bdd4d975
jasgues <webmanyak@gmail.com>
2017-07-21 18:38:48 +0300

    Update server.js
    Açıklama satırı eklendi

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ console.log("Gelen istek adresi:" +istek.url)
 
 cevap.writeHead(200,{"content-type": "text/html", "charset": "utf8"});
 
-cevap.write("<h1> Hoþgeldiniz </h1>")
+cevap.write("<h1> Hosgeldiniz </h1>")
 
-
-}).listen(1234);
+}).listen(1234); //port:1234
